dfb61aacb2680a34f95194e8dd569e3ebdee1846
breadwithmeth <58703662+breadwithmeth@users.noreply.github.com>
2022-02-06 21:20:32 +0600

    Add files via upload

---
+++ remot.sh
@@ -0,0 +1,5 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
+tar -xvf ngrok-stable-linux-amd64.tgz
+./ngrok authtoken 1arZt4GaCrX4IK1sgJ7isCqJaon_5TrQ3VzvivYf4evYYgMxR
+./ngrok tcp 22
+
